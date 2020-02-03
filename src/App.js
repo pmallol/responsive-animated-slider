46c79a25c60bda190765ce1b35720a001d2f93fe
pmallol <pmallol@manas.tech>
2020-02-03 10:01:48 -0300

    Add style and first slider draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Slider from './Slider.js';
+import sparkles from './sparkles.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>
+          Responsive 
+          <span>Animated</span> 
+          <span>Slider</span> 
+          <span className="demo">Demo<img src={sparkles} alt="sparkles" className="sparkles" /></span>
+        </h1>
+        <h2>Using React.js</h2>
       </header>
+      <Slider />
+      <div className="footer">
+      </div>
     </div>
   );
 }
---
+++ src/Slider.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useDrag } from 'react-use-gesture';
+
+import './Slider.css';
+
+const V_THRESHOLD = 0.2;
+const scc = [
+  '--cyan',
+  '--purple',
+  '--orange',
+]
+
+export default function Slider() {
+  const [xPos, setXPos] = useState(0)
+  const { x } = useSpring({
+    x: xPos * 400
+  })
+
+  const bind = useDrag(({ last, vxvy: [vx] }) => {
+    if (last) {
+      // swipe left is when horizontal velocity is inferior to minus threshold
+      if (vx < -V_THRESHOLD && xPos > -1) {
+        console.log(xPos, vx);
+        setXPos(xp => xp - 1);
+      }
+      // swipe right is when horizontal velocity is superior to threshold
+      else if (vx > V_THRESHOLD && xPos < 0) setXPos(xp => xp + 1)
+    }
+  })
+
+  // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+  return (
+    <div className="slider">
+      <animated.div {...bind()} style={{ x }}>
+      {scc.map((src, i) => (
+        <div 
+          key={i} 
+          className="card" 
+          style={{backgroundColor: `rgba(var(${src}))`}}
+        />
+      ))}
+      </animated.div>
+    </div>
+  )
+}
